e44bf1e26cb97a6a28b4f6e819047317369c7c25
rohit2219 <rohit_2219@rediffmail.com>
2017-10-16 06:30:29 -0400

    Linked list add

---
+++ linkedlist/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char x='10000000000';
+    char m='1100';
+    char a=2;
+    char b=6;
+    char y[12];
+	int count=0;
+	
+    for (i=0;i<=strlen(x);i++)
+	{	if (i==a )
+		{	
+			for (j=0;j<=strlen(m);j++)
+			{	
+				x[i]=m[j++]
+				i++
+			}
+		}
+		
+	}
+
+}
